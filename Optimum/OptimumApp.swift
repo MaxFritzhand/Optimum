e5c0ea3949f120ece33bf901f60fb02971525e9e
Max Fritzhand <mfritzhand1@gmail.com>
2023-04-16 22:20:49 -0500

    Initial Commit

---
+++ Optimum/OptimumApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptimumApp.swift
+//  Optimum
+//
+//  Created by Max Fritzhand on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct OptimumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
